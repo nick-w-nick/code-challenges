27ac721b670f4365950cdcde50013a0f5c6ae6e1
nick-w-nick <43578531+nick-w-nick@users.noreply.github.com>
2021-04-13 13:06:38 -0400

    added link to digital_root challenge

--- codewars/digital_root.js
+++ codewars/digital_root.js
@@ -1,3 +1,5 @@
+// https://www.codewars.com/kata/541c8630095125aba6000c00
+
 // Convert a number (85262) to an array ([ 8, 5, 2, 6, 2 ]) for iteration
 const createNumberArray = (num) => {
     const numberArray = num.toString().split('').map(digit => {
