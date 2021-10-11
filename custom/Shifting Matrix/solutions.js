f019614973c09582ebe72880cd6232cb5072da85
nick-w-nick <43578531+nick-w-nick@users.noreply.github.com>
2021-10-11 14:01:43 -0400

    converting shifting_matrix to new format

---
+++ custom/Shifting Matrix/solutions.js
@@ -0,0 +1,13 @@
+exports.solution1 = (rows) => {
+    let initialArray = [];
+    for (let i = 0; i < rows; i++) {
+        let currentRow = [];
+        for (let innerIndex = 0; innerIndex < rows; innerIndex++) {
+            currentRow.push(0);
+        }
+        currentRow[i] = 1;
+        initialArray.push(currentRow);
+    }
+    return initialArray;
+
+};
