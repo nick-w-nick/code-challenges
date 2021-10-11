64dece41ec3c85f5b63afbb62c7680812b77b1cf
nick-w-nick <43578531+nick-w-nick@users.noreply.github.com>
2021-10-11 09:43:46 -0400

    added task descriptions + challenge links

--- codesignal/centuryFromYear.js
+++ codesignal/centuryFromYear.js
@@ -1,3 +1,9 @@
+// https://app.codesignal.com/arcade/intro/level-1/egbueTZRRL5Mm4TXN
+
+// Given a year, return the century it is in.
+// The first century spans from the year 1 up to and including the year 100
+// The second - from the year 101 up to and including the year 200, etc
+
 function centuryFromYear(year) {
     const divided = year / 100;
     return Math.ceil(divided);
--- codesignal/checkPalindrome.js
+++ codesignal/checkPalindrome.js
@@ -1,3 +1,6 @@
+// https://app.codesignal.com/arcade/intro/level-1/s5PbmwxfECC52PWyQ
+// Given the string, check if it is a palindrome
+
 function checkPalindrome(inputString) {
     const stringArray = inputString.split('');
     return stringArray.reverse().join('') === inputString;
